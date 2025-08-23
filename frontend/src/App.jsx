97f57a504b2cd7832565849822513e297c32f202
Unstable108 <rajadas1071@gmail.com>
2025-08-23 22:05:45 +0530

    initilize frontend via vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='flex justify-center'>
+      <ul>
+        <li>Logo</li>
+        <li> 
+          <button>Signup</button>
+        </li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default App
